4c3b5ed2639af5ad611251abe47bf446669d0ab1
andyWynyard <andrew.wynyard@gmail.com>
2018-11-07 14:37:48 +0100

    random user api fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,7 +10,7 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 const path = require(`path`)
 
-// const { get } = require('axios')
+const { get } = require('axios')
 const crypto = require('crypto')
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -58,54 +58,56 @@ exports.createPages = ({ graphql, actions }) => {
   })
 }
 
-// exports.sourceNodes = async ({ actions }) => {
-//   const { createNode } = actions
+exports.sourceNodes = async ({ actions }) => {
+  const { createNode } = actions
 
-//   // fetch raw data from the randomuser api
-//   const fetchRandomUser = () => get(`https://randomuser.me/api/?results=500`)
-//   // await for results
-//   const res = await fetchRandomUser()
+  // fetch raw data from the randomuser api
+  const fetchRandomUser = () => get(`https://randomuser.me/api/`)
+  // await for results
+  const res = await fetchRandomUser().catch(function(error) {
+    console.log('This is an error bro', error)
+  })
 
-//   // map into these results and create nodes
-//   res.data.results.map((user, i) => {
-//     // Create your node object
-//     const userNode = {
-//       // Required fields
-//       id: `${i}`,
-//       parent: `__SOURCE__`,
-//       internal: {
-//         type: `RandomUser`, // name of the graphQL query --> allRandomUser {}
-//         // contentDigest will be added just after
-//         // but it is required
-//       },
-//       children: [],
+  // map into these results and create nodes
+  res.data.results.map((user, i) => {
+    // Create your node object
+    const userNode = {
+      // Required fields
+      id: `${i}`,
+      parent: `__SOURCE__`,
+      internal: {
+        type: `RandomUser`, // name of the graphQL query --> allRandomUser {}
+        // contentDigest will be added just after
+        // but it is required
+      },
+      children: [],
 
-//       // Other fields that you want to query with graphQl
-//       gender: user.gender,
-//       name: {
-//         title: user.name.title,
-//         first: user.name.first,
-//         last: user.name.last,
-//       },
-//       picture: {
-//         large: user.picture.large,
-//         medium: user.picture.medium,
-//         thumbnail: user.picture.thumbnail,
-//       },
-//       // etc...
-//     }
+      // Other fields that you want to query with graphQl
+      gender: user.gender,
+      name: {
+        title: user.name.title,
+        first: user.name.first,
+        last: user.name.last,
+      },
+      picture: {
+        large: user.picture.large,
+        medium: user.picture.medium,
+        thumbnail: user.picture.thumbnail,
+      },
+      // etc...
+    }
 
-//     // Get content digest of node. (Required field)
-//     const contentDigest = crypto
-//       .createHash(`md5`)
-//       .update(JSON.stringify(userNode))
-//       .digest(`hex`)
-//     // add it to userNode
-//     userNode.internal.contentDigest = contentDigest
+    // Get content digest of node. (Required field)
+    const contentDigest = crypto
+      .createHash(`md5`)
+      .update(JSON.stringify(userNode))
+      .digest(`hex`)
+    // add it to userNode
+    userNode.internal.contentDigest = contentDigest
 
-//     // Create node with the gatsby createNode() API
-//     createNode(userNode)
-//   })
+    // Create node with the gatsby createNode() API
+    createNode(userNode)
+  })
 
-//   return
-// }
+  return
+}
